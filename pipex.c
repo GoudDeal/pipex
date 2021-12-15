79288df17121518252b191e7d237d492fad49009
GoudDeal <cypriend0602@gmail.com>
2021-12-15 19:14:07 +0100

    bugfix

--- pipex.c
+++ pipex.c
@@ -6,7 +6,7 @@
 /*   By: user42 <user42@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/06 15:36:42 by dcyprien          #+#    #+#             */
-/*   Updated: 2021/12/15 17:52:13 by user42           ###   ########.fr       */
+/*   Updated: 2021/12/15 19:10:34 by user42           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,61 +48,74 @@ char	*get_path(char *cmd, char **envp)
 	return (NULL);
 }
 
-void	ft_first_command(char *path, char **av, int *pipefd, int *fd, char **envp)
+pid_t	ft_first_command(char **av, int *pipefd, int *fd, char **envp)
 {
 	pid_t	pid;
 	char	**cmd;
+	char	*path;
 
-	cmd = NULL;
 	cmd = ft_split(av[2], ' ');
+	path = get_path(cmd[0], envp);
 	pid = fork();
 	if (pid < 0)
 		terminate("fork");
 	if (pid == 0)
 	{
 		if (fd[0] < 0)
-			terminate("Failed to open infile");
+			terminate("Failed to open input file");
 		close(pipefd[0]);
 		dup2(fd[0], STDIN_FILENO);
 		dup2(pipefd[1], STDOUT_FILENO);
 		if(path && cmd[0])
+		{
 			execve(path, cmd, envp);
+			free_malloc(cmd);
+			free(path);
+		}
 		else
 			error_cmd(cmd[0]);
 	}
+	return (pid);
 }
 
-void	ft_second_command(char *path, char **av, int *pipefd, int *fd, char **envp)
+pid_t	ft_second_command(char **av, int *pipefd, int *fd, char **envp)
 {
 	pid_t		pid;
 	char		**cmd;
+	char		*path;
 
-	cmd = NULL;
 	cmd = ft_split(av[3], ' ');
+	path = get_path(cmd[0], envp);
 	pid = fork();
 	if (pid < 0)
 		terminate("fork");
 	if (pid == 0)
 	{
 		if (fd[1] < 0)
-			terminate("Failed to open outfile");
+			terminate("Failed to open output file");
 		close(pipefd[1]);
 		dup2(fd[1], STDOUT_FILENO);
 		dup2(pipefd[0], STDIN_FILENO);
 		if(path && cmd[0])
+		{
 			execve(path, cmd, envp);
+			free_malloc(cmd);
+			free(path);
+		}
 		else
 			error_cmd(cmd[0]);
 	}
+	write(2, "on arrive bien là ?\n", 22);
+	return(pid);
 }
 
 int	main(int ac, char **av, char **envp)
 {
-	int		fds[2];
-	char	*cmd_one;
-	char	*cmd_two;
-	int		pipefd[2];
-	int		status;
+	int			fds[2];
+	int			pipefd[2];
+	int			status;
+	pid_t		first_pid;
+	pid_t		second_pid;
 
 	if (ac != 5)
 		usage();
@@ -112,16 +125,16 @@ int	main(int ac, char **av, char **envp)
 			terminate(NULL);
 		fds[0] = open(av[1], O_RDONLY | O_CREAT, 0644);
 		fds[1] = open(av[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
-		cmd_one = get_path(av[2], envp);
-		cmd_two = get_path(av[3], envp);
-		ft_first_command(cmd_one, av, pipefd, fds, envp);
-		ft_second_command(cmd_two, av, pipefd, fds, envp);
-		free(cmd_one);
-		free(cmd_two);
+		first_pid = ft_first_command(av, pipefd, fds, envp);
+		second_pid = ft_second_command(av, pipefd, fds, envp);
 		close(fds[0]);
 		close(fds[1]);
-		waitpid(-1, &status, 0);
-		waitpid(-1, &status, 0);
+		waitpid(first_pid, &status, 0);
+		write(2, "on sort \n", 10);
+		status = 0;
+		printf("%d\n", (int)second_pid);
+		waitpid(second_pid, &status, 0);
+		write(2, "on sort pas\n", 13);
 	}
 	return (0);
 }
